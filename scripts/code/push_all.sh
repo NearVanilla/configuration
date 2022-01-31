1d32befc507127c08e4b130f0c9950ef1c90cb3b
Professor Bloodstone <git@bloodstone.dev>
2022-01-31 02:14:28 +0100

    Add script to push latest changes, ignoring latest commit if substituted

---
+++ scripts/code/push_all.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set -x
+
+git_root="$(git rev-parse --show-toplevel)"
+cd "${git_root}"
+
+is_subst() {
+  local -r ref="${1:-HEAD}"
+  git log -n1 --format=%s "${ref}" | grep --fixed-strings '[SUBST]' >/dev/null
+}
+
+current_branch() {
+  git rev-parse --abbrev-ref HEAD
+}
+
+get_sha() {
+  local -r ref="${1:-HEAD}"
+  git log -n1 --format=%H "${ref}"
+}
+
+for repo in . ./website/files/ ./server-config/*/; do
+  cd "${git_root}/${repo}"
+  ref=HEAD
+  ! is_subst "${ref}" || ref='HEAD^'
+  ! is_subst "${ref}" || {
+    printf "Two commits substituted in %s?\n" "${repo}"
+    exit 1
+  }
+  branch="$(current_branch)"
+  [ "${branch}" != "HEAD" ] || {
+    printf 'Unable to check the branch in %s\n' "${repo}"
+    exit 1
+  }
+  local_sha="$(get_sha "${ref}")"
+  remote_sha="$(get_sha "origin/${branch}")"
+  [ "${local_sha}" = "${remote_sha}" ] || git push origin "${ref}":"${branch}"
+done
